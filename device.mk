96333be04acaef010dddaa684554313df8f1a610
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-04-08 21:42:02 +0530

    ginkgo: Drop unnecessarily overriden blobs
    build/make/core/Makefile:49: warning: overriding commands for target `/home/adithya/wave/out/target/product/ginkgo/system/system_ext/framework/vendor.qti.hardware.capabilityconfigstore-V1.0-java.jar'
    build/make/core/base_rules.mk:496: warning: ignoring old commands for target `/home/adithya/wave/out/target/product/ginkgo/system/system_ext/framework/vendor.qti.hardware.capabilityconfigstore-V1.0-java.jar'
    build/make/core/Makefile:49: warning: overriding commands for target `/home/adithya/wave/out/target/product/ginkgo/system/system_ext/lib/vendor.qti.hardware.capabilityconfigstore@1.0.so'
    build/make/core/base_rules.mk:496: warning: ignoring old commands for target `/home/adithya/wave/out/target/product/ginkgo/system/system_ext/lib/vendor.qti.hardware.capabilityconfigstore@1.0.so'
    build/make/core/Makefile:49: warning: overriding commands for target `/home/adithya/wave/out/target/product/ginkgo/system/system_ext/lib64/vendor.qti.hardware.capabilityconfigstore@1.0.so'
    build/make/core/base_rules.mk:496: warning: ignoring old commands for target `/home/adithya/wave/out/target/product/ginkgo/system/system_ext/lib64/vendor.qti.hardware.capabilityconfigstore@1.0.so'
    build/make/core/Makefile:49: warning: overriding commands for target `/home/adithya/wave/out/target/product/ginkgo/vendor/lib/libwfdaac_vendor.so'
    build/make/core/base_rules.mk:496: warning: ignoring old commands for target `/home/adithya/wave/out/target/product/ginkgo/vendor/lib/libwfdaac_vendor.so'

--- device.mk
+++ device.mk
@@ -458,6 +458,7 @@ PRODUCT_PACKAGES += \
     libqdMetaData \
     libqdMetaData.system \
     libqdMetaData.vendor \
+    libwfdaac_vendor \
     vendor.display.config@2.0
 
 PRODUCT_BOOT_JARS += \
