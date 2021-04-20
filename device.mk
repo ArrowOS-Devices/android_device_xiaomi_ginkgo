805f9d994121c7466ba20c0f79abb75aaee89768
Bruno Martins <bgcngm@gmail.com>
2021-04-20 21:56:48 +0100

    ginkgo: Build offline charging daemon dependency
    This must be explicitly built as of R. Fixes off mode charging.
    
    Change-Id: Ie013d25d55105b4eb2191b9fd625ed8e02c863c5
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -106,6 +106,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Charger
+PRODUCT_PACKAGES += \
+    libsuspend
+
 # Config Store
 PRODUCT_PACKAGES += \
     disable_configstore
