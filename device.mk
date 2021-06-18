c912c03b3e9e09d8c145f8d11d653f55eeb1dd2b
Arian <arian.kulmer@web.de>
2021-06-18 18:41:53 +0200

    ginkgo: Import powerhint configuration from sunfish
    powerhint.json is checked out to https://android.googlesource.com/device/google/sunfish/+/refs/tags/android-11.0.0_r38/powerhint.json.
    
    Change-Id: Ib576cea82363865cefd5e5fa2e7c746c0b95c3a8
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -319,7 +319,8 @@ PRODUCT_PACKAGES += \
 
 # Perf
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
+    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QTI VNDK Framework Detect
 PRODUCT_PACKAGES += \
