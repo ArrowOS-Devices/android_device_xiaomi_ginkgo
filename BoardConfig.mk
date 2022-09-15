de6bd843ae1e23f71d7927a54109b37b8c29e8dd
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-09-15 16:39:45 +0530

    ginkgo: Switch to latest version of sdclang 12
    Adapt to restructured upstream repo.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,7 +111,7 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/ginkgo
 TARGET_KERNEL_CONFIG := vendor/ginkgo-perf_defconfig
 
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := sdllvm
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/vendor/qcom/sdclang/compiler
 TARGET_KERNEL_ADDITIONAL_FLAGS := LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
