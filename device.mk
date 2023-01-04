c2d41b0766cf620ac28b100c4f26a19c3ea0167d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-01-04 13:57:09 +0000

    ginkgo: Build shims from hardware/arrow/compat
    Shims that are commonly used among devices have moved to
    https://github.com/ArrowOS/android_hardware_arrow_compat.
    
    While we're at it, stop building the system variants as
    nothing is using them.

--- device.mk
+++ device.mk
@@ -186,15 +186,11 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.base@1.0.vendor \
+    android.hidl.manager@1.0.vendor
 
 PRODUCT_PACKAGES += \
-    libhidltransport \
     libhidltransport.vendor \
-    libhwbinder \
     libhwbinder.vendor
 
 # IMS
